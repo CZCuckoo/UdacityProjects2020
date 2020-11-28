3144926f50df1fe23d30dbb4eceee092fdddd536
Clint Johnson <CJohnson@illuminatetraining.local>
2020-11-28 13:06:36 -0500

    Added AJAX calls

--- CRUD_App/app.py
+++ CRUD_App/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #creates an application that gets named after the name of our file
@@ -11,10 +11,20 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     
-    def __repr__J(self):
+    def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description': todo.description
+    })
 
 @app.route('/')
 def index():
